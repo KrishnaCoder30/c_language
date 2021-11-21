3bbfc47266499e2c84dcb33e2665ef9ab3021d55
KrishnaCoder30 <93732516+KrishnaCoder30@users.noreply.github.com>
2021-11-21 13:43:47 +0530

    Update Delete A Element of desire Position.c

--- Array/Delete A Element of desire Position.c
+++ Array/Delete A Element of desire Position.c
@@ -44,9 +44,7 @@ int main()
 
  		printf("\nNew Elements in Descending Order are :");
 
- 		for(i=0;i<n-1;i++)
-
- 		{
+ 		
 
  			for(j=0;j<n-1;j++)
 
@@ -73,6 +71,113 @@ int main()
  		
 
  		for(i=0;i<n;i++)
+
+ 		{
+
+ 			
+
+ 			
+
+ 		
+
+ 		
+
+ 		
+
+ 		
+
+
+#include <stdio.h>
+
+ 
+
+int main()
+
+{
+
+ int a[100];
+
+ int i,j,k,n;
+
+ scanf("%d",&n);
+
+ printf("No. of Elements: %d\n",n);
+
+ for(i=0;i<n;i++)
+
+ {
+
+ 	scanf("%d",&a[i]);
+
+ 	printf("\nElement %d : %d ",i,a[i]);
+
+ 		}
+
+ 	scanf("%d",&k);
+
+ 	printf("\nPosition Of Element to be Removed : %d\n",k);
+
+ 	if(k<n)
+
+ 	{
+
+ 	for(i=k-1;i<n-1;i++)
+
+ 	{
+
+ 		a[i]=a[i+1];
+
+ 		}
+
+ 		for(i=0;i<n-1;i++)
+
+ 	{
+
+ 		printf("%d ",a[i]);
+
+ 		}
+
+ 		}
+
+ 		else
+
+ 		{
+
+ 			printf("Invalid Position");
+
+ 			}
+
+ 		printf("\nNew Elements in Descending Order are :");
+
+ 		for(i=0;i<n-1;i++)
+
+ 		{
+
+ 			for(j=0;j<n-2;j++)
+
+ 			{
+
+ 				if(a[j]<=a[j+1])
+
+ 				{
+
+ 					k=a[j+1];
+
+ 					a[j+1]=a[j];
+
+ 					a[j]=k;
+
+ 					
+
+ 					}
+
+ 			}
+
+ 		}
+
+ 		
+
+ 		for(i=0;i<n-1;i++)
 
  		{
 
